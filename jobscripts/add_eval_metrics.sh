40fc9e0a21b2ee0a171a9b5143d587cfc35d9684
S4410653 <sijbren.vv@outlook.com>
2025-02-19 16:02:49 +0100

    Update add_eval_metrics.sh: Fix output path.

--- jobscripts/add_eval_metrics.sh
+++ jobscripts/add_eval_metrics.sh
@@ -4,5 +4,5 @@
 # SBCSAE
 ## flan-t5-xl
 ### "Complete this sentence: "
-python3 add_eval_metrics.py -inp "exp/completion/SBCSAE/flan-t5-xl/flan-t5-xl_cts_fine-tune_chrf.json" -out "exp/SBCSAE/flan-t5-xl_nopx_fine-tune_chrf"
+python3 add_eval_metrics.py -inp "exp/completion/SBCSAE/flan-t5-xl/flan-t5-xl_cts_fine-tune_chrf.json" -out "exp/completion/SBCSAE/flan-t5-xl_cts_fine-tune_chrf"
 
